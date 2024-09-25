83cf890c1ddb289cce783e5614d7193cdae4b0f9
listas-nivelamento-main <fernando.candido@aluno.ifsp.edu.br>
2024-09-24 22:36:26 -0300

    refector: ajuste no metodo ColarFoto da classe album,valida valor <= 0

--- src/main/java/org/exercicio/Album.java
+++ src/main/java/org/exercicio/Album.java
@@ -10,8 +10,9 @@ public class Album {
     }
 
     public boolean ColarFotos(int L1, int H1, int L2, int H2) {
-        if (L1 < 0 || H1 < 0 || L2  < 0 || H2 < 0) {
+        if (L1 <= 0 || H1 <= 0 || L2 <= 0 || H2 <= 0) {
             return false;
+
         }
         return this.Encaixe(L1, H1, L2, H2) || this.Encaixe(L1, H1, H2, L2) || this.Encaixe(H1, L1, L2, H2) || this.Encaixe(H1, L1, H2, L2);
     }
