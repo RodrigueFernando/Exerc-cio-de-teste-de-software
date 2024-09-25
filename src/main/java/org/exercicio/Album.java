e426e66071e2a499effc5108928e94d8ac1946ae
listas-nivelamento-main <fernando.candido@aluno.ifsp.edu.br>
2024-09-24 21:01:07 -0300

    refector: ajuste no classe album

--- src/main/java/org/exercicio/Album.java
+++ src/main/java/org/exercicio/Album.java
@@ -1,2 +1,19 @@
-package org.exercicio;public class Album {
+package org.exercicio;
+
+public class Album {
+    private int larguraPagina;
+    private int alturaPagina;
+
+    public Album(int largura, int altura) {
+        this.larguraPagina = largura;
+        this.alturaPagina = altura;
+    }
+
+    public boolean ColarFotos(int L1, int H1, int L2, int H2) {
+        return this.Encaixe(L1, H1, L2, H2) || this.Encaixe(L1, H1, H2, L2) || this.Encaixe(H1, L1, L2, H2) || this.Encaixe(H1, L1, H2, L2);
+    }
+
+    private boolean Encaixe(int L1, int H1, int L2, int H2) {
+        return L1 + L2 <= this.larguraPagina && Math.max(H1, H2) <= this.alturaPagina || H1 + H2 <= this.alturaPagina && Math.max(L1, L2) <= this.larguraPagina;
+    }
 }
