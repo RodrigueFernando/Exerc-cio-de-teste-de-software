05eb9d42922e15b1ee6aa7c76855ac064c44b28c
listas-nivelamento-main <fernando.candido@aluno.ifsp.edu.br>
2024-09-24 21:04:42 -0300

    feat:implementação da classe main

--- src/main/java/org/exercicio/Main.java
+++ src/main/java/org/exercicio/Main.java
@@ -1,7 +1,29 @@
 package org.exercicio;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("largura da pagina");
+        int X = scanner.nextInt();
+        System.out.println("Altura da página");
+        int Y = scanner.nextInt();
+        System.out.println("Largura da primeira foto");
+        int L1 = scanner.nextInt();
+        System.out.println("Altura da primeira foto");
+        int H1 = scanner.nextInt();
+        System.out.println("Largura da segunda foto");
+        int L2 = scanner.nextInt();
+        System.out.println("Largura da segunda foto");
+        int H2 = scanner.nextInt();
+        Album Album = new Album(X, Y);
+        if (Album.ColarFotos(L1, H1, L2, H2)) {
+            System.out.println("S");
+        } else {
+            System.out.println("N");
+        }
+
     }
+
 }
