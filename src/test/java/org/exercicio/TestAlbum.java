13fbc064a62fd2bbfa3eafadc2d0d8d6f3d20e92
listas-nivelamento-main <fernando.candido@aluno.ifsp.edu.br>
2024-09-24 21:51:14 -0300

    feat: metodo testa fotos validas

--- src/test/java/org/exercicio/TestAlbum.java
+++ src/test/java/org/exercicio/TestAlbum.java
@@ -1,4 +1,23 @@
+package org.exercicio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 class TestAlbum {
-  
+    @Test
+    @DisplayName("Teste fotos quecabem na página")
+    public void testFotosCabemValidas() {
+        Album pagina = new Album(5, 7);
+        assertTrue(pagina.ColarFotos(3,4,3,4));
+        //assertThat(pagina.ColarFotos(3, 4, 3, 4), equalTo(true));
+
+    }
+
+
+
 }
